4c6668222b044a0e7aae20c72e98276cb277be2f
Aimin Tang <aimin.tang@gmail.com>
2018-05-05 10:43:09 -0500

    add5mul3

---
+++ js/js_novice_to_ninja/ch7/main.js
@@ -0,0 +1,27 @@
+function doSomething(event){
+    console.log(`screen: (${event.screenX},${event.screenY}), page: (${event.pageX},${event.pageY}), client: (${event.screenX},${event.screenY})`)
+}
+
+addEventListener('click', doSomething);
+
+const clickParagraph = document.getElementById('click');
+
+clickParagraph.addEventListener('click',() => console.log('click') );
+clickParagraph.addEventListener('mousedown',() => console.log('down') );
+clickParagraph.addEventListener('mouseup',() => console.log('up') );
+
+const dblclickParagraph = document.getElementById('dblclick');
+dblclickParagraph.addEventListener('dblclick', highlight);
+
+function highlight(event) {
+    event.target.classList.toggle('highlight');
+}
+
+const mouseParagraph = document.getElementById('mouse');
+mouseParagraph.addEventListener('mouseover', highlight);
+mouseParagraph.addEventListener('mouseout', highlight);
+mouseParagraph.addEventListener('mousemove', () =>  console.log('You Moved!') );
+
+addEventListener('keydown',highlight);
+addEventListener('keyup', (event) => console.log(`You stopped pressing the key on ${new Date}`));
+addEventListener('keypress', (event) => console.log(`You pressed the ${event.key} character`));
