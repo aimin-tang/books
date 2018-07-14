18711669eb3825344a4e95368dd880ec1f939cb3
Aimin Tang <aimin.tang@gmail.com>
2018-07-14 05:57:10 -0500

    update

--- js/modern_javascript_from_the_beginning/ch04/app.js
+++ js/modern_javascript_from_the_beginning/ch04/app.js
@@ -8,6 +8,9 @@ loadEventListeners();
 
 function loadEventListeners() {
     form.addEventListener('submit', addTask);
+    taskList.addEventListener('click', removeTask);
+    clearBtn.addEventListener('click', clearTasks);
+    filter.addEventListener('keyup', filterTasks);
 }
 
 function addTask(e) {
@@ -20,11 +23,37 @@ function addTask(e) {
     li.appendChild(document.createTextNode(taskInput.value));
     const link = document.createElement('a');
     link.className = 'delete-item secondary-content';
-    link.innerHTML = '<i class="fa fa-remove"></i>';
+    link.innerHTML = '<i class="fa fa-trash"></i>';
     li.appendChild(link);
 
     taskList.appendChild(li);
     taskInput.value = '';
 
     e.preventDefault();
+}
+
+function removeTask(e) {
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        if (confirm('Are you sure?')) {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+function clearTasks(e) {
+    while (taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+    document.querySelectorAll('.collection-item').forEach(function(task) {
+        const item = task.firstChild.textContent;
+        if (item.toLowerCase().indexOf(text) !== -1) {
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    });
 }
