f457ae7498c09812f3fecbc7ab2af714a7a2867b
Aimin Tang <aimin.tang@gmail.com>
2018-10-06 06:14:10 -0500

    icndb_json.groovy

---
+++ groovy/icndb_json.groovy
@@ -0,0 +1,7 @@
+import groovy.json.*
+
+String base = 'http://api.icndb.com/jokes/random?'
+def qs = [limitTo: '[nerdy]'].collect { k, v -> "$k=$v" }.join('&')
+String jsonTxt = "$base$qs".toURL().text
+def json = new JsonSlurper().parseText(jsonTxt)
+println json.value.joke
